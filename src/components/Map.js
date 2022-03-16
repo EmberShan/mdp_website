72dcd215bc3f9220fa445ff1d2253482d3d9b7b2
ember2000 <zshan@email.wm.edu>
2022-03-16 10:41:25 -0400

    added constraints and change dots location relative to the map

--- src/components/Map.js
+++ src/components/Map.js
@@ -7,44 +7,46 @@ const Map = ({toggled}) => {
     const img1 = require('./imgs/first_floor.png')
     const img2 = require('./imgs/second_floor.png')
     // coordinates of the markers
-    const [start, setStart] = useState({x: 20, y: 30});
-    const [dest, setDest] = useState({x: 50, y: 30});
+    const [start, setStart] = useState({x: 0, y: 0});
+    const [dest, setDest] = useState({x: 100, y: 100});
 
     //test functions for user location
     const moveUp = () => {
-        setStart({ x: start.x, y: start.y + 5 });
+        setStart({ x: start.x, y: Math.min(100, Math.max(0, start.y + 5)) });
     }
     const moveDown = () => {
-        setStart({ x: start.x, y: start.y - 5 });
+        setStart({ x: start.x, y: Math.min(100, Math.max(0, start.y - 5)) });
     }
     const moveLeft = () => {
-        setStart({ x: start.x + 5, y: start.y });
+        setStart({ x: Math.min(100, Math.max(0, start.x + 5)), y: start.y });
     }
     const moveRight = () => {
-        setStart({ x: start.x - 5, y: start.y });
+        setStart({  x: Math.min(100, Math.max(0, start.x - 5)), y: start.y });
     }
     //test functions for destination location
     const moveUp2 = () => {
-        setDest({ x: dest.x, y: dest.y + 5 });
+        setDest({ x: dest.x, y: Math.min(100, Math.max(0, dest.y + 5)) });
     }
     const moveDown2 = () => {
-        setDest({ x: dest.x, y: dest.y - 5 });
+        setDest({ x: dest.x, y: Math.min(100, Math.max(0, dest.y - 5)) });
     }
     const moveLeft2 = () => {
-        setDest({ x: dest.x + 5, y: dest.y });
+        setDest({ x: Math.min(100, Math.max(0, dest.x + 5)), y: dest.y });
     }
     const moveRight2 = () => {
-        setDest({ x: dest.x - 5, y: dest.y });
+        setDest({  x: Math.min(100, Math.max(0, dest.x - 5)), y: dest.y });
     }
     
 
     return(
-        <div className="map">
-            <span className="marker" style = {{ top: `${start.y}%`, left: `${start.x}%`, backgroundColor: 'red'}}></span>
-            <span className="marker" style = {{ top: `${dest.y}%`, left: `${dest.x}%`, backgroundColor: 'blue' }}></span>
+        <div className="container">
+            <div className="map" style={{position: 'relative'}}>
+                <span className="marker" style = {{ top: `${start.y}%`, left: `${start.x}%`, backgroundColor: 'red'}}></span>
+                <span className="marker" style = {{ top: `${dest.y}%`, left: `${dest.x}%`, backgroundColor: 'blue' }}></span>
 
-            <img className ="floor-img" src={toggled ? img1 : img2}/>
-
+                <img className ="floor-img" src={toggled ? img1 : img2}/>
+            </div>
+            
             {/* dugging buttons */}
             <div className='debug'>
                 <div>
