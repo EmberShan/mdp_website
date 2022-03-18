7c880bfe6bbba7bee1c7c23d8e7b2e97957cba29
ember2000 <zshan@email.wm.edu>
2022-03-18 09:01:58 -0400

    changed map from function to class

--- src/components/Map.js
+++ src/components/Map.js
@@ -2,29 +2,30 @@ import React from "react";
 import { useState } from 'react';
 import '../css/Map.css';
 
-const Map = ({toggled}) => {
-    // imgs of the map
-    const img1 = require('./imgs/demo.png')
-    const img2 = require('./imgs/second_floor.png')
-    // coordinates of the markers
-    const [start, setStart] = useState({x: 0, y: 0});
-    const [dest, setDest] = useState({x: 100, y: 100});
 
-    //change location of marker like this
-    const moveUp = () => {
-        setStart({ x: start.x, y: Math.min(100, Math.max(0, start.y + 5)) });
+class Map extends React.Component {
+    constructor(props){
+        super(props);
+        this.state = {
+            // imgs of the map
+            img1: require('./imgs/demo.png'),
+            img2: require('./imgs/second_floor.png'),
+            // coordinates of the markers
+            start: [0, 0],
+            des: [0, 0],
+        }
+    };
+
+    render(){
+        return(
+            <div className="map" style={{position: 'relative'}}>
+                <span className="marker" style = {{ top: `${this.state.start[1]}%`, left: `${this.state.start[0]}%`, backgroundColor: 'red'}}></span>
+                <span className="marker" style = {{ top: `${this.state.dest[1]}%`, left: `${this.state.dest[0]}%`, backgroundColor: 'blue' }}></span>
+
+                <img className ="floor-img" src={this.props.toggled ? this.state.img1 : this.state.img2}/>
+            </div>
+        )
     }
-
-    return(
-        <div className="map" style={{position: 'relative'}}>
-            <span className="marker" style = {{ top: `${start.y}%`, left: `${start.x}%`, backgroundColor: 'red'}}></span>
-            <span className="marker" style = {{ top: `${dest.y}%`, left: `${dest.x}%`, backgroundColor: 'blue' }}></span>
-
-            <img className ="floor-img" src={toggled ? img1 : img2}/>
-        </div>
-
-    )
-
 }
 
 export default Map
