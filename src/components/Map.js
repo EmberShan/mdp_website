d59858be3f46d37fa52a788c25057dfdaeac9798
ember2000 <zshan@email.wm.edu>
2022-03-18 09:23:23 -0400

    cleaned up and detected the dimensions of img

--- src/components/Map.js
+++ src/components/Map.js
@@ -1,28 +1,40 @@
 import React from "react";
-import { useState } from 'react';
 import '../css/Map.css';
 
+// imgs of the map
+const img1 = require('./imgs/demo.png');
+const img2 = require('./imgs/second_floor.png');
 
 class Map extends React.Component {
     constructor(props){
         super(props);
         this.state = {
-            // imgs of the map
-            img1: require('./imgs/demo.png'),
-            img2: require('./imgs/second_floor.png'),
-            // coordinates of the markers
+            // coordinates of the markers; for now there are only two
             start: [0, 0],
-            des: [0, 0],
-        }
+            dest: [100, 100],
+            // size of img
+            sizeOfImg: {},
+        };
+        // functions here
+        this.onImgLoad = this.onImgLoad.bind(this);
     };
+
+    // detect the size of the image
+    onImgLoad({target:img}){
+        this.setState({sizeOfImg: {w: img.offsetWidth, h: img.offsetHeight}});
+        console.log('w:' + img.offsetWidth + 'px', 'h:' + img.offsetHeight + 'px');
+    }
 
     render(){
         return(
             <div className="map" style={{position: 'relative'}}>
+
+                {/* dots / map markers */}
                 <span className="marker" style = {{ top: `${this.state.start[1]}%`, left: `${this.state.start[0]}%`, backgroundColor: 'red'}}></span>
                 <span className="marker" style = {{ top: `${this.state.dest[1]}%`, left: `${this.state.dest[0]}%`, backgroundColor: 'blue' }}></span>
-
-                <img className ="floor-img" src={this.props.toggled ? this.state.img1 : this.state.img2}/>
+                {/* img */}
+                <img className ="floor-img" onLoad={this.onImgLoad} src={this.props.toggled ? img1 : img2}/>
+            
             </div>
         )
     }
