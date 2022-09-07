0d613ac18e139f3043028204fd576b892c83fcc2
ember2000 <zshan@email.wm.edu>
2022-09-07 11:11:05 -0400

    added search bar

--- src/components/Body/Body.js
+++ src/components/Body/Body.js
@@ -3,23 +3,47 @@ import { useState } from 'react';
 import './Body.scss';
 import Map from '../Map/Map';
 
-const Body = ({}) => {
+// wrapping everything in the screen 
+const Body = ({ }) => {
     const [toggled, setToggled] = useState(true)
 
-    const switchFloor = () =>{
+    const switchFloor = () => {
         console.log(toggled);
         setToggled((state) => (state === true ? false : true));
     }
 
     return (
         <body>
-            <Map toggled={toggled}/>
+
+            {/* fixed */}
+            <span className='searchBarWrapper'>
+                <input type="text" placeholder="search for a location..." />
+                <button>
+                    <i class="fa-solid fa-magnifying-glass"></i>
+                </button>
+            </span>
+
+            <Map toggled={toggled} />
+            
+            {/* fixed button group */}
+            <span className='btnWrapper'>
+                <span className='flootBtn'>
+                    <button>
+                        2F
+                    </button>
+                    <button>
+                        1F
+                    </button>
+                </span>
+
+                <button></button>
+            </span>
 
             {/* <button onClick={switchFloor} className="switchFloorBtn"> 
                 {toggled ? "Switch to Second Floor" : "Switch to First Floor"} 
             </button> */}
         </body>
-        
+
     )
 }
 
